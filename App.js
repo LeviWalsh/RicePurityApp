217b7878f819904a170c75a7bafe585b72c845c4
LeviWalsh <leviwalsh@berkeley.edu>
2022-02-08 11:40:03 -0800

    example navigator working as desired, need to change out for my screens

--- App.js
+++ App.js
@@ -1,14 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import IntroScreen from './app/screens/IntroScreen';
 import QuizScreen from './app/screens/QuizScreen';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+function LogoTitle() {
+  return (
+    <View
+      style={{ backgroundColor: "yellow", width: 50, height: 50 }}
+      
+    />
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <IntroScreen />
+    <NavigationContainer>
+      <Stack.Navigator 
+        initialRouteName='Home'
+        screenOptions={{headerShown: false}}
+      >
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen}
+          options={{ title: 'My Home' }} />
+        <Stack.Screen 
+          name="Details" 
+          component={DetailsScreen}
+          options={{ headerTitle: (props) => <LogoTitle /> }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
